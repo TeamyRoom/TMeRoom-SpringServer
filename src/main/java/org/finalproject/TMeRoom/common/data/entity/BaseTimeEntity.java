769364f791559a77586122375163ed146a33d000
TaeMin Kim <tk4003950@gmail.com>
2023-09-11 16:30:20 +0900

    ✨	: BaseTimeEntity 추가
    - 엔티티 추가 시간 및 수정 시간 작성의 편의를 위해 JpaAuditing 기능을 사용해 BaseTimeEntity 작성함

---
+++ src/main/java/org/finalproject/TMeRoom/common/data/entity/BaseTimeEntity.java
@@ -0,0 +1,33 @@
+package org.finalproject.TMeRoom.common.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * 작성자: 김태민
+ * 작성일자: 2023-09-11
+ * 모든 엔티티에 작성 일자와 수정 일자를 자동으로 표기하기 위한 엔티티입니다.
+ * 엔티티 작성시 extends 키워드로 해당 클래스를 상속해 사용할 수 있습니다.
+ */
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+@NoArgsConstructor
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+}
