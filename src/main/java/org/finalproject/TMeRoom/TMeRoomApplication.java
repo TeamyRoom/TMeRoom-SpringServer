78cb2a9ecc40c7db75de2212f6d4c0521a0061e8
whdals9711 <70644199+whdals9711@users.noreply.github.com>
2023-09-11 13:39:09 +0900

    🎉	: 프로젝트 시작

---
+++ src/main/java/org/finalproject/TMeRoom/TMeRoomApplication.java
@@ -0,0 +1,13 @@
+package org.finalproject.TMeRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMeRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TMeRoomApplication.class, args);
+	}
+
+}
