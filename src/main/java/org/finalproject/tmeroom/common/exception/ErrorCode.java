48e825090cabdb6067b736588a6e31e0a1414c2d
TaeMin Kim <tk4003950@gmail.com>
2023-09-12 17:29:23 +0900

    ✨	: 에러 코드와 메시지를 관리할 ErrorCode Enum 클래스 작성

---
+++ src/main/java/org/finalproject/tmeroom/common/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package org.finalproject.tmeroom.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 작성자: 김태민
+ * 작성일자: 2023-09-12
+ * 에러 코드와 에러 메시지를 관리하기 위한 Enum 클래스
+ */
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+//    ==== 여기서부터 작성 ====
+
+//    ==== 여기까지 작성 ====
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
