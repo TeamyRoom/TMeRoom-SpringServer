80314b7e62501fcf1ee1950e8e4ca5e4cf922c2d
TaeMin Kim <tk4003950@gmail.com>
2023-09-12 17:00:43 +0900

    ✨	: 인증 성공시 회원 정보를 담을 DTO 작성

--- src/main/java/org/finalproject/tmeroom/member/data/dto/MemberDto.java
+++ src/main/java/org/finalproject/tmeroom/member/data/dto/MemberDto.java
@@ -1,10 +1,52 @@
 package org.finalproject.tmeroom.member.data.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.finalproject.tmeroom.member.constant.MemberRole;
+import org.finalproject.tmeroom.member.data.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
 public class MemberDto {
-    String id;
+
+    @ToString.Include
+    private String id;
+    private String pw;
+    @ToString.Include
+    private String nickname;
+    @ToString.Include
+    private String email;
+    @ToString.Include
+    private MemberRole role;
+
+    @Builder
+    private MemberDto(String id, String nickname, String pw, String email, MemberRole role) {
+        this.id = id;
+        this.pw = pw;
+        this.nickname = nickname;
+        this.email = email;
+        this.role = role;
+    }
+
+    public static MemberDto from(Member member) {
+        return MemberDto.builder()
+                .id(member.getId())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .pw(member.getPw())
+                .build();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> role.getName());
+        return authorities;
+    }
 }
