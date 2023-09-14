50233ee6f778606c8de2c63bbff6b076f2f8d916
TaeMin Kim <tk4003950@gmail.com>
2023-09-14 11:56:40 +0900

    ✨	: 인증 관련 에러 코드 추가

--- src/main/java/org/finalproject/tmeroom/common/exception/ErrorCode.java
+++ src/main/java/org/finalproject/tmeroom/common/exception/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     //    ==== 여기서부터 작성 ====
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
     INVALID_PERMISSION(HttpStatus.FORBIDDEN, "소유자 불일치"),
-    //    ==== 여기까지 작성 ====
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."),
+    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "비밀번호가 일치하지 않습니다."),
+//    ==== 여기까지 작성 ====
     ;
 
     private final HttpStatus status;
