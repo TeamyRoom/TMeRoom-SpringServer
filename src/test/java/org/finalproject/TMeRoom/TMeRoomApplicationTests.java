2380b6a79a1834916570ed8fc385b31be015eec4
TaeMin Kim <tk4003950@gmail.com>
2023-09-18 12:20:00 +0900

    🔧	: local 과 test 환경 나누기 위해 application.yml 수정

--- src/test/java/org/finalproject/TMeRoom/TMeRoomApplicationTests.java
+++ src/test/java/org/finalproject/TMeRoom/TMeRoomApplicationTests.java
@@ -2,12 +2,14 @@ package org.finalproject.TMeRoom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TMeRoomApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
