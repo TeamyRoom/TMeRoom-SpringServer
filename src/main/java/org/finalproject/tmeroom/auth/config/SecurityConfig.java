8681a68160fe3aeb856e7dbb291007e4a80fa285
TaemHam <tk4003950@gmail.com>
2023-09-20 15:07:10 +0900

    ✨	: 인증, 인가 실패 처리를 담당하는 클래스를 보안 필터에 추가

--- src/main/java/org/finalproject/tmeroom/auth/config/SecurityConfig.java
+++ src/main/java/org/finalproject/tmeroom/auth/config/SecurityConfig.java
@@ -12,19 +12,31 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * 작성자: 김태민
+ * 작성일자: 2023-09-13
+ * 보안 관련 필터 설정
+ * 요청이 컨트롤러에 닿기 전에 인증, 인가 및 여러 보안 문제를 사전에 차단하는 기능을 함
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic(AbstractHttpConfigurer::disable)
+
                 // REST API는 CSRF 대응 필요 없음
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -32,14 +44,22 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
 
                 // 세션 관리 비활성화
-                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(
+                        (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 // URI 인증, 인가 설정
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/v1/lecture/**").authenticated()
                         .anyRequest().permitAll())
 
                 // JWT 인증필터 등록
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler))
+        ;
 
         return http.build();
     }
