bbe243323a098a2ec8ce15c829a9c7ed83193611
TaemHam <tk4003950@gmail.com>
2023-09-20 12:54:58 +0900

    ✨	: 이메일 인증 컨트롤러 로직 추가

--- src/main/java/org/finalproject/tmeroom/member/controller/MemberController.java
+++ src/main/java/org/finalproject/tmeroom/member/controller/MemberController.java
@@ -2,12 +2,10 @@ package org.finalproject.tmeroom.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.finalproject.tmeroom.common.data.dto.Response;
+import org.finalproject.tmeroom.member.data.dto.request.EmailConfirmRequestDto;
 import org.finalproject.tmeroom.member.data.dto.request.MemberCreateRequestDto;
 import org.finalproject.tmeroom.member.service.MemberService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -19,6 +17,19 @@ public class MemberController {
     @PostMapping
     public Response<Void> createMember(@RequestBody MemberCreateRequestDto requestDto) {
         memberService.createMember(requestDto);
+        return Response.success();
+    }
+
+    @PostMapping("/email/confirm")
+    public Response<Void> confirmMailTemp(@RequestBody EmailConfirmRequestDto requestDto) {
+        memberService.confirmEmail(requestDto.getConfirmCode());
+        return Response.success();
+    }
+
+    // TODO: 프론트 없을 때 테스트 용도. 프론트 완료되면 삭제 예정
+    @GetMapping("/confirm/{confirm-code}")
+    public Response<Void> confirmMailTemp(@PathVariable("confirm-code") String confirmCode) {
+        memberService.confirmEmail(confirmCode);
         return Response.success();
     }
 }
