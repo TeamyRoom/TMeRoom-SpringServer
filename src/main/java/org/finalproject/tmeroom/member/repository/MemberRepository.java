0cae426bbab20003436ef33e215847c3d765414e
TaemHam <tk4003950@gmail.com>
2023-09-24 16:19:00 +0900

    ✨	: Member Repository에서 이메일로 회원을 조회하는 기능 추가

--- src/main/java/org/finalproject/tmeroom/member/repository/MemberRepository.java
+++ src/main/java/org/finalproject/tmeroom/member/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package org.finalproject.tmeroom.member.repository;
 import org.finalproject.tmeroom.member.data.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
