a6d895088690d19b5dc4fc2842499533d39189aa
TaeMin Kim <tk4003950@gmail.com>
2023-10-11 16:50:36 +0900

    🎨	: application.yaml을 개인 repo에 보관하기 위해 삭제

--- src/test/java/org/finalproject/TMeRoom/auth/config/jwt/JwtTokenProviderTest.java
+++ src/test/java/org/finalproject/TMeRoom/auth/config/jwt/JwtTokenProviderTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,7 +22,7 @@ import static org.mockito.BDDMockito.given;
 
 
 @SpringBootTest(classes = {JwtTokenProvider.class})
-@TestPropertySource(properties = {"JWT_KEY=LongLongLongLongLongLongLongLongTestJWTKey"})
+@ActiveProfiles("test")
 @DisplayName("JWT 관련 테스트")
 class JwtTokenProviderTest {
 
