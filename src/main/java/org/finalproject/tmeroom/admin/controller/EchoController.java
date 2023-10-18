9779d340fee7fe71cf045bdacda286dcd983859b
TaeMin Kim <tk4003950@gmail.com>
2023-10-18 11:27:56 +0900

    🎨	: EchoController Import 문 정리

--- src/main/java/org/finalproject/tmeroom/admin/controller/EchoController.java
+++ src/main/java/org/finalproject/tmeroom/admin/controller/EchoController.java
@@ -1,22 +1,10 @@
 package org.finalproject.tmeroom.admin.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.finalproject.tmeroom.admin.constant.LectureSearchType;
-import org.finalproject.tmeroom.admin.constant.MemberRoleSearchType;
-import org.finalproject.tmeroom.admin.constant.MemberSearchType;
-import org.finalproject.tmeroom.admin.data.dto.request.*;
-import org.finalproject.tmeroom.admin.data.dto.response.AdminLectureDetailReadResponseDto;
-import org.finalproject.tmeroom.admin.data.dto.response.AdminLecturePageReadResponseDto;
-import org.finalproject.tmeroom.admin.data.dto.response.AdminMemberDetailReadResponseDto;
-import org.finalproject.tmeroom.admin.data.dto.response.AdminMemberPageReadResponseDto;
-import org.finalproject.tmeroom.admin.service.AdminService;
 import org.finalproject.tmeroom.common.data.dto.Response;
-import org.finalproject.tmeroom.member.data.dto.MemberDto;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
