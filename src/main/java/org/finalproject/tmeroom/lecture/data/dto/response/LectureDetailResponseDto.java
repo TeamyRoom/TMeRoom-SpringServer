f576855b96dbbd538596d9f74cb3556b4f3d786b
KJY-kpu <rntlls1366@gmail.com>
2023-10-24 11:41:05 +0900

    ⚡️	: 수강 중인 강의 조회 시 수강신청이 되었는지 판별

--- src/main/java/org/finalproject/tmeroom/lecture/data/dto/response/LectureDetailResponseDto.java
+++ src/main/java/org/finalproject/tmeroom/lecture/data/dto/response/LectureDetailResponseDto.java
@@ -4,21 +4,26 @@ import lombok.Builder;
 import lombok.Getter;
 import org.finalproject.tmeroom.lecture.data.entity.Student;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class LectureDetailResponseDto {
     String lectureCode;
     String lectureName;
+    LocalDateTime acceptedAt;
 
     @Builder
-    public LectureDetailResponseDto(String lectureCode, String lectureName) {
+    public LectureDetailResponseDto(String lectureCode, String lectureName, LocalDateTime acceptedAt) {
         this.lectureCode = lectureCode;
         this.lectureName = lectureName;
+        this.acceptedAt = acceptedAt;
     }
 
     public static LectureDetailResponseDto from(Student student) {
         return LectureDetailResponseDto.builder()
                 .lectureCode(student.getLectureCode())
                 .lectureName(student.getLecture().getLectureName())
+                .acceptedAt(student.getAcceptedAt())
                 .build();
     }
 }
